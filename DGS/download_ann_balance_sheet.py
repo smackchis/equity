364b119786c9db197ffc9443abbf00c8645dbad1
smackchis <62486147+smackchis@users.noreply.github.com>
2020-03-29 23:29:14 +0530

    quarterly balance sheet initial commit

--- DGS/download_ann_balance_sheet.py
+++ DGS/download_ann_balance_sheet.py
@@ -3,7 +3,7 @@ import datahandler
 import pandas as pd
 
 sym_list_query = "use wm; select symbol from dbo.symbol_list;"
-bal_sheet_db_table = 'dbo.ann_bal_sheet'
+ann_bal_sheet_db_table = 'dbo.ann_bal_sheet'
 
 df = datahandler.read_db_data(sym_list_query)
 for index, row in df.iterrows():
@@ -12,4 +12,4 @@ for index, row in df.iterrows():
     yahoo_financials = YahooFinancials(sym)
     AnnualBalSheetData = yahoo_financials.get_financial_stmts('annual', 'balance')
     print(AnnualBalSheetData)
-    datahandler.write_fin_to_db(AnnualBalSheetData, bal_sheet_db_table)
+    datahandler.write_fin_to_db(AnnualBalSheetData, ann_bal_sheet_db_table)
